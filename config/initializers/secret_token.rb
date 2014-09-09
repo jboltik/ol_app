d28543945e45ecd2d504230eff13ff5d4926f5c5
Jacqueline Boltik <jboltik@gmail.com>
2014-09-09 15:12:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlApp::Application.config.secret_token = '14ca02ddcbf56d372ceeae28a316e00bee80daccc3b2663d142c9fa7d52b36bbc04c66ac551b89ade50d9050142479968c04cb188bd5310fdf02e6ef9e11fb53'
